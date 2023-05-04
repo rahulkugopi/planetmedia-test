a054fa41d36d98fd65818209d51b5bfb32d0e5cb
rahulkugopi <rahulkgopi@gmail.com>
2023-05-04 12:50:25 +0530

    api new changes

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate } from 'react-router-dom';
 import api from '../../constants/Api';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Blog() {
 
@@ -19,15 +20,15 @@ function Blog() {
       });
   }
 
-  const handleReadMore = () => {
-    navigate("/blog-details/1");
+  const handleReadMore = (evt) => {
+    navigate(`/blog-details/${evt}`);
   }
 
   console.log(blogData)
 
   return (
     <div data-testid='blog'>
-      <div className='blog-banner bg-slate-300 flex items-center justify-center bg-no-repeat bg-cover' style={{ backgroundImage: "url(https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops.png)" }}>
+      <div className='blog-banner bg-slate-300 flex items-center justify-center bg-no-repeat bg-cover' style={{ backgroundImage: "url(https://source.unsplash.com/random/?office&€7)" }}>
         <h2 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white'>Blog</h2>
       </div>
 
@@ -38,12 +39,18 @@ function Blog() {
             blogData.map((res, index) => (
               <>
                 <div className='shadow pt-5 pb-5 pr-5 pl-5'>
-                  <img src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/blog/office-laptops.png' />
-                  <h2 className='pt-5 text-1xl font-semibold leading-none tracking-tight dark:text-white'>{res.title}</h2>
-                  <p className='pt-2 '>{res.body}</p>
+                  <div className='blog-top-img flex align-middle justify-center'>
+                    <img src={`${process.env.REACT_APP_IMG_URL}/?office&€${index}`} className='object-cover' />
+                  </div>
+                  <h2 className='pt-5 text-1xl font-semibold leading-noneindex tracking-tight dark:text-white capitalize'>{res.title}</h2>
+                  <div className='blog-content'> 
+                    <div className='blog-content-inner'>
+                    <p className='pt-2'>{res.body}</p>
+                    </div>                                      
+                  </div>
                   <div className='flex pt-5'>
                     <div className='w-12'>
-                      <img src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png' className='rounded-full object-cover' />
+                      <AccountCircleIcon style={{ fontSize: '45px' }} />
                     </div>
 
                     <p className='ml-5'>
@@ -52,7 +59,7 @@ function Blog() {
                       <span className='text-sm'>Aug 15, 2021 · 16 min read</span>
                     </p>
                   </div>
-                  <button id="button" className="mt-8 bg-transparent text-blue duration-300" onClick={handleReadMore}>
+                  <button id="button" className="mt-8 bg-transparent text-blue duration-300" onClick={e => handleReadMore(index)}>
                     Read more <ArrowForwardIcon style={{ fontSize: '16px' }} />
                   </button>
                 </div>
